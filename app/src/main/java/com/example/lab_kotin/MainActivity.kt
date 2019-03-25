aad80cd82543d1ab34cda87b176ca70287cf4742
kamonthep <kamonthep026@gmail.com>
2019-03-25 15:39:50 +0700

    page login

--- app/src/main/java/com/example/lab_kotin/MainActivity.kt
+++ app/src/main/java/com/example/lab_kotin/MainActivity.kt
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var btnLogin: Button? = null
+    var etIdEmail: EditText? = null
+    var etPass : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
+        if (v == etidEmail){
+
+
+        }
         if (v === btnLogin) {
             Toast.makeText(this@MainActivity, "รหัสผ่านผิดพลาด", Toast.LENGTH_SHORT).show()
         }
